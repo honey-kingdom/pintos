219b68941a69f6476c5dc8da0e2dcc3c44c358ef
kdh6024 <kdh6024@gmail.com>
2018-01-22 23:25:16 +0900

    Project 0 Success

---
+++ src/tests/threads/print-name.c
@@ -0,0 +1,20 @@
+/* Creates N threads, each of which sleeps a different, fixed
+   duration, M times.  Records the wake-up order and verifies
+   that it is valid. */
+
+#include <stdio.h>
+#include "tests/threads/tests.h"
+#include "threads/init.h"
+#include "threads/malloc.h"
+#include "threads/synch.h"
+#include "threads/thread.h"
+#include "devices/timer.h"
+
+void
+test_print_name (void)
+{
+    printf("(print-name) Course : SSE3044\n");
+    printf("(print-name) ID     : 2009311648\n");
+    printf("(print-name) Name   : Dong-Hyeon Kim\n");
+}
+
