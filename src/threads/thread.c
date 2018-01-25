bf4b5b64085e62a5b20f08005b4adeedea3e5325
kdh6024 <kdh6024@gmail.com>
2018-01-25 21:55:03 +0900

    added waiting list in thread system

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -28,6 +28,10 @@ static struct list ready_list;
    when they are first scheduled and removed when they exit. */
 static struct list all_list;
 
+/* List of processes in THREAD_BLOCKED state, that is, processes
+   that are waiting for their timers to expire */
+static struct list waiting_list;
+
 /* Idle thread. */
 static struct thread *idle_thread;
 
@@ -92,6 +96,7 @@ thread_init (void)
   lock_init (&tid_lock);
   list_init (&ready_list);
   list_init (&all_list);
+  list_init (&waiting_list);
 
   /* Set up a thread structure for the running thread. */
   initial_thread = running_thread ();
